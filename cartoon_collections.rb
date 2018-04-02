a102190474f2e43524234fef752b948e2a7d2eaf
Diego Yanez <diegosyanez92@gmail.com>
2018-04-02 15:19:25 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,7 +6,7 @@ def roll_call_dwarves (array)# code an argument here
 
 end
 
-def summon_captain_planet (array)# code an argument here
+def summon_captain_planet(array)# code an argument here
   # Your code here
   array.map {|caps| caps.capitalize }
   caps.each { |upcase| puts upcase + "!"}
