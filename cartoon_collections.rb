08a402ea2cc87757f1bace497162aa58f4146676
Diego Yanez <diegosyanez92@gmail.com>
2018-04-02 15:22:51 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,7 +9,7 @@ end
 def summon_captain_planet(array)# code an argument here
   array.map { |new_array| new_array.capitalize  }
   array.each { |new_array| puts new_array + "!"}
-  array.length
+
 end
 
 def long_planeteer_calls# code an argument here
