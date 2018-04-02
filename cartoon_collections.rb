d31510a13652e41fac780a8b7665283f5c074a13
Diego Yanez <diegosyanez92@gmail.com>
2018-04-02 15:56:24 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,7 +12,7 @@ def summon_captain_planet(array) # code an argument here
 end
 
 def long_planeteer_calls (array)# code an argument here
-  if array.length > 4 ? true : false
+  if array.length > 4 true : false
   end
 
 end
