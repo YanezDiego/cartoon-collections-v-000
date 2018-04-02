e53c3bc6a833184dbbd0effb5ada11d175b0e73c
Diego Yanez <diegosyanez92@gmail.com>
2018-04-02 16:31:03 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -22,4 +22,5 @@ def find_the_cheese (array)# code an argument here
   cheese_types = ["cheddar", "gouda", "camembert"]
   array.find do |maybe_chesse|
   cheeses_type.include?(maybe_chesse)
+  end
 end
