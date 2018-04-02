d803bbf91ef8384eb2cc2c6605cd9ea74b2298ae
Diego Yanez <diegosyanez92@gmail.com>
2018-04-02 16:01:35 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,7 +18,7 @@ def long_planeteer_calls (array)# code an argument here
 end
 
 def find_the_cheese (array)# code an argument here
-  
+  array.include?(cheese_types)
 
 
   # the array below is here to help
