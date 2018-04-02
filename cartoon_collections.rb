6635a566d677a5f9b4a6933a83ed30949496d9dc
Diego Yanez <diegosyanez92@gmail.com>
2018-04-02 15:31:17 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,18 +1,19 @@
 require 'pry'
-def roll_call_dwarves (array)# code an argument here
+def roll_call_dwarves (array) # code an argument here
   array.each.with_index(1) do |value, index|
     puts "#{index}. #{value}"
   end
 
 end
 
-def summon_captain_planet(array)# code an argument here
+def summon_captain_planet(array) # code an argument here
   array.map { |new_array| new_array.capitalize + "!" }
 
 end
 
-def long_planeteer_calls# code an argument here
-  
+def long_planeteer_calls # code an argument here
+
+
 end
 
 def find_the_cheese# code an argument here
