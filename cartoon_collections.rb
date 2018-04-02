ada4bb2f0d55dd74d02aed87df76c2b78070661d
Diego Yanez <diegosyanez92@gmail.com>
2018-04-02 16:30:24 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,9 +18,8 @@ def long_planeteer_calls (array)# code an argument here
 end
 
 def find_the_cheese (array)# code an argument here
-
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-
-  array.include?('cheddar')
+  array.find do |maybe_chesse|
+  cheeses_type.include?(maybe_chesse)
 end
