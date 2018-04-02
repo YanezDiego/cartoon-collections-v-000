2f17919d96183f279d930e6c5a554c38859c9c13
Diego Yanez <diegosyanez92@gmail.com>
2018-04-02 15:19:28 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,7 +8,7 @@ end
 
 def summon_captain_planet(array)# code an argument here
   # Your code here
-  array.map {|caps| caps.capitalize }
+  array.map {|caps| caps.capitalize}
   caps.each { |upcase| puts upcase + "!"}
 end
 
